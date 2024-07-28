25122cd319f2006858173e96f05aef917e200e59
kevinbacino <kevin.bacino@gmail.com>
2024-07-28 08:09:11 -0400

    validation on the fly

--- src/app/components/intl-phone/intl-phone.component.ts
+++ src/app/components/intl-phone/intl-phone.component.ts
@@ -3,9 +3,10 @@ import {
   AbstractControl,
   UntypedFormBuilder,
   UntypedFormGroup,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
-import { getAsYouType } from 'awesome-phonenumber';
+import { getAsYouType, getExample } from 'awesome-phonenumber';
 
 @Component({
   selector: 'app-intl-phone',
@@ -14,6 +15,7 @@ import { getAsYouType } from 'awesome-phonenumber';
 })
 export class IntlPhoneComponent implements AfterViewInit {
   public form: UntypedFormGroup;
+  public placeholderPhone: string;
   public countries: string[] = ['US', 'SE', 'MX'];
 
   constructor(private formBuilder: UntypedFormBuilder) {
@@ -21,11 +23,24 @@ export class IntlPhoneComponent implements AfterViewInit {
       phone: [''],
       select: [this.countries[0]],
     });
+    const examplePhone = getExample(this.countries[0]);
+    this.placeholderPhone = examplePhone.number?.national || '';
   }
 
   ngAfterViewInit() {
     this.form.controls['phone'].addValidators(Validators.required);
     this.form.controls['phone'].addValidators(this.phoneValid);
+    this.form.controls['phone'].addValidators(
+      this.validateMax(() => this.form.controls['select'].value),
+    );
+  }
+
+  public newCountryChange() {
+    this.form.controls['phone'].setValue('');
+    const examplePhone = getExample(this.form.controls['select'].value);
+    this.placeholderPhone = examplePhone.number?.national || '';
+    this.form.markAsPristine();
+    this.form.markAsUntouched();
   }
 
   public phoneInput(event: any) {
@@ -48,13 +63,36 @@ export class IntlPhoneComponent implements AfterViewInit {
     );
   }
 
-  public validationError() {
+  public phoneMaxLengthError() {
+    const control = this.form.controls['phone'];
+    return (
+      control.status !== 'VALID' &&
+      control.touched === true &&
+      control.errors?.['maxLengthError']
+    );
+  }
+
+  public phoneAytError() {
     const control = this.form.controls['phone'];
     return (
       control.status !== 'VALID' &&
       control.touched === true &&
       control.errors?.['aytIssues']
     );
+  }
+
+  private validateMax(func: any): ValidatorFn {
+    return (c: AbstractControl) => {
+      const value = c.value || '';
+      const max = func() === 'US' ? 10 : 12;
+      const isNotValid = value.replace(/[^0-9]/g, '').length > max;
+      if (isNotValid) {
+        return {
+          maxLengthError: true,
+        };
+      }
+      return null;
+    };
   }
 
   private phoneValid(control: AbstractControl) {
