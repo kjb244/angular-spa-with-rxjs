f4d01da03b9678519af0eaa553721df8a3fd39a0
kjb244 <kevin.bacino@gmail.com>
2024-09-03 06:38:52 -0400

    fixed toPromise since it's deprecated in v8

--- src/app/guards/synchronous/master.synch.guard.ts
+++ src/app/guards/synchronous/master.synch.guard.ts
@@ -2,6 +2,7 @@ import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular
 import {ContentSynchGuard} from "./content.synch.guard";
 import {MvpSynchGuard} from "./mvp.synch.guard";
 import {Injectable} from "@angular/core";
+import {firstValueFrom} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,7 @@ export class MasterSynchGuard implements CanActivate{
     const guardsMapping = ['content guard', 'mvp guard']
     for(let i=0; i< guards.length; i++){
       console.log('about to call', guardsMapping[i])
-      const result = await guards[i].canActivate(route, state).toPromise();
+      const result = await firstValueFrom(guards[i].canActivate(route, state));
       console.log( guardsMapping[i], 'finishing with', result)
       if(!result){
         return false;
